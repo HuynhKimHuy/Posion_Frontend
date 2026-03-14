9344b53cde4e74905af749fcda0504329e276b5e
HuynhKimHuy <Huynhkimhuy69@gmal.com>
2026-03-15 04:19:17 +0700

    config header and footer chat layout

--- src/components/chat/ChatWindowLayout.tsx
+++ src/components/chat/ChatWindowLayout.tsx
@@ -8,28 +8,28 @@ import ChatWindowBody from "./ChatWindowBody"
 
 
 const ChatWindowLayout = () => {
-    const {activeConversationId,
+    const { activeConversationId,
         conversations,
         messages,
-        messagesLoading:loading,
+        messagesLoading: loading,
 
     } = useChatStore()
     const selectedConversation = conversations.find(c => c._id === activeConversationId) ?? null
-    if(!selectedConversation){
-        return <ChatWellcome/>
+    if (!selectedConversation) {
+        return <ChatWellcome />
     }
-    if(loading || !messages[selectedConversation._id]){
+    if (loading || !messages[selectedConversation._id]) {
         // return <ChatWindowSkeleton/>
     }
     return (
         <SidebarInset className="flex h-full min-h-0 flex-1 flex-col overflow-hidden rounded-sm shadow-md">
-           {/* <ChatWindowHeader/> */}
-           <ChatWindowHeader chat={selectedConversation}/>       
-           {/* <ChatWindowBody/> */}
-                     <div className="flex-1 min-h-0 overflow-hidden">
-                         <ChatWindowBody/>
-                     </div>
-              {/* <ChatWindowFooter/> */}
+            {/* <ChatWindowHeader/> */}
+            <ChatWindowHeader chat={selectedConversation} />
+            {/* <ChatWindowBody/> */}
+            <div className="flex-1 min-h-0 overflow-hidden">
+                <ChatWindowBody />
+            </div>
+            {/* <ChatWindowFooter/> */}
             <ChatWindowFooter />
         </SidebarInset>
     )
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import { SidebarProvider } from "@/components/ui/sidebar"
 import ChatWindowLayout from "@/components/chat/ChatWindowLayout"
 
 const HomePage = () => {
-  
+
   return (
     <SidebarProvider className="h-svh max-h-svh overflow-hidden">
       <AppSidebar />
